4a7a8023871b204908e3fae54a585bf72c72164e
Alejandro Pereira <alepereira86@gmail.com>
2013-08-30 17:29:46 -0300

    Pep8/pyflakes.

--- pdbfinder.py
+++ pdbfinder.py
@@ -28,11 +28,12 @@ DEFAULT_SETTINGS = {
 
 Message = namedtuple('Message', 'type, msg')
 
-## LOGGING SETUP
+# LOGGING SETUP
 try:
     from logging import NullHandler
 except ImportError:
     class NullHandler(logging.Handler):
+
         def handle(self, record):
             pass
 
@@ -43,7 +44,7 @@ except ImportError:
             self.lock = None
 
 log = logging.getLogger('sublime-pdb-finder')
-log.handlers = [] ## hack to prevent extraneous handlers on ST2 auto-reload
+log.handlers = []  # hack to prevent extraneous handlers on ST2 auto-reload
 log.addHandler(NullHandler())
 log.setLevel(logging.INFO)
 if DEBUG:
@@ -54,21 +55,25 @@ if DEBUG:
 def do_when(conditional, callback, *args, **kwargs):
     if conditional():
         return callback(*args, **kwargs)
-    sublime.set_timeout(functools.partial(do_when, conditional, callback, *args, **kwargs), 50)
+    sublime.set_timeout(
+        functools.partial(do_when, conditional, callback, *args, **kwargs), 50)
 
 
 class Settings(dict):
+
     """Combine default and user settings"""
+
     def __init__(self, user_settings):
         settings = DEFAULT_SETTINGS.copy()
         settings.update(user_settings)
-        ## Combine core_patterns and patterns
+        # Combine core_patterns and patterns
         settings['core_patterns'].update(settings['patterns'])
         settings['patterns'] = settings.pop('core_patterns')
         super(Settings, self).__init__(settings)
 
 
 class ThreadProgress(object):
+
     def __init__(self, thread, message, success_message, file_counter):
         self.thread = thread
         self.message = message
@@ -88,8 +93,8 @@ class ThreadProgress(object):
 
         before = i % self.size
         after = (self.size - 1) - before
-        sublime.status_message('%s [%s=%s] (%s files scanned)' % \
-            (self.message, ' ' * before, ' ' * after, self.file_counter))
+        sublime.status_message('%s [%s=%s] (%s files scanned)' %
+                              (self.message, ' ' * before, ' ' * after, self.file_counter))
         if not after:
             self.addend = -1
         if not before:
@@ -98,10 +103,11 @@ class ThreadProgress(object):
         sublime.set_timeout(lambda: self.run(i), 100)
 
 
-
 class PdbfinderExtractor(object):
-    def __init__(self, settings, filepaths, dirpaths, ignored_dirs, ignored_file_patterns, 
-                 file_counter):
+
+    def __init__(
+        self, settings, filepaths, dirpaths, ignored_dirs, ignored_file_patterns,
+            file_counter):
         self.filepaths = filepaths
         self.dirpaths = dirpaths
         self.patterns = settings['patterns']
@@ -127,8 +133,7 @@ class PdbfinderExtractor(object):
         for dirpath in dirs:
             dirpath = path.abspath(dirpath)
             for dirpath, dirnames, filenames in walk(dirpath):
-                ## remove excluded dirs
-                ## TODO: These are not patterns. Consider making them glob patterns
+
                 for dir in exclude_dirs:
                     if dir in dirnames:
                         self.log.debug(u'Ignoring dir: {0}'.format(dir))
@@ -156,7 +161,8 @@ class PdbfinderExtractor(object):
     def extract(self):
         """"""
         message_patterns = '|'.join(self.patterns.values())
-        case_sensitivity = 0 if self.settings.get('case_sensitive', False) else re.IGNORECASE
+        case_sensitivity = 0 if self.settings.get(
+            'case_sensitive', False) else re.IGNORECASE
         patt = re.compile(message_patterns, case_sensitivity)
         for filepath in self.search_targets():
             try:
@@ -171,14 +177,15 @@ class PdbfinderExtractor(object):
                     try:
                         next_pdb_line = enum_file.next()
                     except StopIteration:
-                        next_pdb_line = (None, '')
+                        next_pdb_line = (None, "EOF")
                     for mo in patt.finditer(line):
-                        ## Remove the non-matched groups
-                        matches = [Message(msg_type, next_pdb_line[1]) for msg_type, msg in mo.groupdict().iteritems()]
+                        # Remove the non-matched groups
+                        matches = [Message(msg_type, next_pdb_line[1])
+                                   for msg_type, msg in mo.groupdict().iteritems()]
                         for match in matches:
                             yield {'filepath': filepath, 'linenum': linenum + 1, 'match': match}
             except IOError:
-                ## Probably a broken symlink
+                # Probably a broken symlink
                 f = None
             finally:
                 self.file_counter.increment()
@@ -187,6 +194,7 @@ class PdbfinderExtractor(object):
 
 
 class PdbfinderRenderer(object):
+
     def __init__(self, settings, window, file_counter):
         self.window = window
         self.settings = settings
@@ -207,7 +215,7 @@ class PdbfinderRenderer(object):
 
     @property
     def view(self):
-        existing_results = [v for v in self.window.views() 
+        existing_results = [v for v in self.window.views()
                             if v.name() == self.view_name and v.is_scratch()]
         if existing_results:
             v = existing_results[0]
@@ -232,7 +240,8 @@ class PdbfinderRenderer(object):
             if matches:
                 yield ('header', u'\n## {0} ({1})'.format(message_type.upper().decode('utf8', 'ignore'), len(matches)), {})
                 for idx, m in enumerate(matches, 1):
-                    msg = m['match'].msg.decode('utf8', 'ignore') ## Don't know the file encoding
+                    # Don't know the file encoding
+                    msg = m['match'].msg.decode('utf8', 'ignore')
                     filepath = path.basename(m['filepath'])
                     line = u"{idx}. {filepath}:{linenum} --> next line: {msg}".format(
                         idx=idx, filepath=filepath, linenum=m['linenum'], msg=msg)
@@ -240,17 +249,17 @@ class PdbfinderRenderer(object):
 
     def render_to_view(self, formatted_results):
         """This blocks the main thread, so make it quick"""
-        ## Header
+        # Header
         result_view = self.view
         edit = result_view.begin_edit()
         result_view.erase(edit, sublime.Region(0, result_view.size()))
         result_view.insert(edit, result_view.size(), self.header)
         result_view.end_edit(edit)
 
-        ## Region : match_dicts
+        # Region : match_dicts
         regions = {}
 
-        ## Result sections
+        # Result sections
         for linetype, line, data in formatted_results:
             edit = result_view.begin_edit()
             insert_point = result_view.size()
@@ -263,11 +272,13 @@ class PdbfinderRenderer(object):
 
         result_view.add_regions('results', regions.keys(), '')
 
-        d_ = dict(('{0},{1}'.format(k.a, k.b), v) for k, v in regions.iteritems())
+        d_ = dict(('{0},{1}'.format(k.a, k.b), v)
+                  for k, v in regions.iteritems())
         result_view.settings().set('result_regions', d_)
 
-        ## Set syntax and settings
-        result_view.set_syntax_file('Packages/sublime-pdb-finder/pdbfinder-results.hidden-tmLanguage')
+        # Set syntax and settings
+        result_view.set_syntax_file(
+            'Packages/sublime-pdb-finder/pdbfinder-results.hidden-tmLanguage')
         result_view.settings().set('line_padding_bottom', 2)
         result_view.settings().set('line_padding_top', 2)
         result_view.settings().set('word_wrap', False)
@@ -276,24 +287,27 @@ class PdbfinderRenderer(object):
 
 
 class WorkerThread(threading.Thread):
+
     def __init__(self, extractor, renderer):
         self.extractor = extractor
         self.renderer = renderer
         threading.Thread.__init__(self)
 
     def run(self):
-        ## Extract in this thread
+        # Extract in this thread
         pdbs = self.extractor.extract()
         rendered = list(self.renderer.format(pdbs))
 
-        ## Render into new window in main thread
+        # Render into new window in main thread
         def render():
             self.renderer.render_to_view(rendered)
         sublime.set_timeout(render, 10)
 
 
 class FileScanCounter(object):
+
     """Thread-safe counter used to update the status bar"""
+
     def __init__(self):
         self.ct = 0
         self.lock = threading.RLock()
@@ -321,7 +335,7 @@ class PdbfinderCommand(sublime_plugin.TextCommand):
     def search_paths(self, window, open_files_only=False):
         """Return (filepaths, dirpaths)"""
         return (
-            [view.file_name() for view in window.views() if view.file_name()], 
+            [view.file_name() for view in window.views() if view.file_name()],
             window.folders() if not open_files_only else []
         )
 
@@ -329,23 +343,27 @@ class PdbfinderCommand(sublime_plugin.TextCommand):
         window = self.view.window()
         settings = Settings(self.view.settings().get('pdbfinder', {}))
 
-        ## TODO: Cleanup this init code. Maybe move it to the settings object
-        filepaths, dirpaths = self.search_paths(window, open_files_only=open_files_only)
+        filepaths, dirpaths = self.search_paths(
+            window, open_files_only=open_files_only)
 
         ignored_dirs = settings.get('folder_exclude_patterns', [])
-        ## Get exclude patterns from global settings
-        ## Is there really no better way to access global settings?
+        # Get exclude patterns from global settings
+        # Is there really no better way to access global settings?
         global_settings = sublime.load_settings('Global.sublime-settings')
         ignored_dirs.extend(global_settings.get('folder_exclude_patterns', []))
 
         exclude_file_patterns = settings.get('file_exclude_patterns', [])
-        exclude_file_patterns.extend(global_settings.get('file_exclude_patterns', []))
-        exclude_file_patterns.extend(global_settings.get('binary_file_patterns', []))
-        exclude_file_patterns = [fnmatch.translate(patt) for patt in exclude_file_patterns]
+        exclude_file_patterns.extend(
+            global_settings.get('file_exclude_patterns', []))
+        exclude_file_patterns.extend(
+            global_settings.get('binary_file_patterns', []))
+        exclude_file_patterns = [fnmatch.translate(patt)
+                                 for patt in exclude_file_patterns]
 
         file_counter = FileScanCounter()
-        extractor = PdbfinderExtractor(settings, filepaths, dirpaths, ignored_dirs, 
-                                  exclude_file_patterns, file_counter)
+        extractor = PdbfinderExtractor(
+            settings, filepaths, dirpaths, ignored_dirs,
+            exclude_file_patterns, file_counter)
         renderer = PdbfinderRenderer(settings, window, file_counter)
 
         worker_thread = WorkerThread(extractor, renderer)
@@ -368,8 +386,9 @@ class NavigateResults(sublime_plugin.TextCommand):
             sublime.status_message('No results to navigate')
             return
 
-        ##NOTE: numbers stored in settings are coerced to floats or longs
-        selection = int(settings.get('selected_result', self.STARTING_POINT[direction]))
+        # NOTE: numbers stored in settings are coerced to floats or longs
+        selection = int(
+            settings.get('selected_result', self.STARTING_POINT[direction]))
         selection = selection + self.DIRECTION[direction]
         try:
             target = results[selection]
@@ -378,37 +397,42 @@ class NavigateResults(sublime_plugin.TextCommand):
             selection = 0
 
         settings.set('selected_result', selection)
-        ## Create a new region for highlighting
+        # Create a new region for highlighting
         target = target.cover(target)
         view.add_regions('selection', [target], 'selected', 'dot')
         view.show(target)
 
 
 class ClearSelection(sublime_plugin.TextCommand):
+
     def run(self, edit):
         self.view.erase_regions('selection')
         self.view.settings().erase('selected_result')
 
 
 class GotoComment(sublime_plugin.TextCommand):
+
     def __init__(self, *args):
         self.log = logging.getLogger('sublime-pdb-finder.nav')
         super(GotoComment, self).__init__(*args)
 
     def run(self, edit):
-        ## Get the idx of selected result region
+        # Get the idx of selected result region
         selection = int(self.view.settings().get('selected_result', -1))
-        ## Get the region
+        # Get the region
         selected_region = self.view.get_regions('results')[selection]
-        ## Convert region to key used in result_regions (this is tedious, but 
-        ##    there is no other way to store regions with associated data)
-        data = self.view.settings().get('result_regions')['{0},{1}'.format(selected_region.a, selected_region.b)]
+        # Convert region to key used in result_regions (this is tedious, but
+        # there is no other way to store regions with associated data)
+        data = self.view.settings().get('result_regions')[
+            '{0},{1}'.format(selected_region.a, selected_region.b)]
         self.log.debug(u'Goto comment at {filepath}:{linenum}'.format(**data))
         new_view = self.view.window().open_file(data['filepath'])
-        do_when(lambda: not new_view.is_loading(), lambda: new_view.run_command("goto_line", {"line": data['linenum']}))
+        do_when(lambda: not new_view.is_loading(), lambda:
+                new_view.run_command("goto_line", {"line": data['linenum']}))
 
 
 class MouseGotoComment(sublime_plugin.TextCommand):
+
     def __init__(self, *args):
         self.log = logging.getLogger('sublime-pdb-finder.nav')
         super(MouseGotoComment, self).__init__(*args)
@@ -425,11 +449,13 @@ class MouseGotoComment(sublime_plugin.TextCommand):
     def run(self, edit):
         if not self.view.settings().get('result_regions'):
             return
-        ## get selected line
+        # get selected line
         pos = self.view.sel()[0].end()
         result = self.get_result_region(pos)
         self.highlight(result)
-        data = self.view.settings().get('result_regions')['{0},{1}'.format(result.a, result.b)]
+        data = self.view.settings().get('result_regions')[
+            '{0},{1}'.format(result.a, result.b)]
         self.log.debug(u'Goto comment at {filepath}:{linenum}'.format(**data))
         new_view = self.view.window().open_file(data['filepath'])
-        do_when(lambda: not new_view.is_loading(), lambda: new_view.run_command("goto_line", {"line": data['linenum']}))
+        do_when(lambda: not new_view.is_loading(), lambda:
+                new_view.run_command("goto_line", {"line": data['linenum']}))
